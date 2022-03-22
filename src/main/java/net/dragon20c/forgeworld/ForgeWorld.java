ceef8797fd47ef3b6cfadb006bcbff289b4c93eb
Dragon20C <Dragon212c@gmail.com>
2022-03-22 18:15:51 +0000

    Added stuff

--- src/main/java/net/dragon20c/forgeworld/ForgeWorld.java
+++ src/main/java/net/dragon20c/forgeworld/ForgeWorld.java
@@ -1,5 +1,8 @@
 package net.dragon20c.forgeworld;
 
+import com.sun.jna.WString;
+import net.dragon20c.forgeworld.block.ModBlocks;
+import net.dragon20c.forgeworld.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,14 +11,12 @@ public class ForgeWorld implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("forgeworld");
+	public static final String MOD_ID = "forgeworld";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
