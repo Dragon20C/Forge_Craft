324dea940547262e8a1403d51b5e76513681ed0d
Dragon20C <Dragon212c@gmail.com>
2022-03-22 18:17:06 +0000

    Added stuff

---
+++ src/main/java/net/dragon20c/forgeworld/block/ModBlocks.java
@@ -0,0 +1,9 @@
+package net.dragon20c.forgeworld.block;
+
+import net.dragon20c.forgeworld.ForgeWorld;
+
+public class ModBlocks {
+    public static void registerModBlocks() {
+        ForgeWorld.LOGGER.info("Registering ModBlocks for" + ForgeWorld.MOD_ID);
+    }
+}
---
+++ src/main/java/net/dragon20c/forgeworld/item/ModItems.java
@@ -0,0 +1,22 @@
+package net.dragon20c.forgeworld.item;
+
+import net.dragon20c.forgeworld.ForgeWorld;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+
+public class ModItems {
+    public static final Item MUD_STICK = registerItem("mud_stick",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(ForgeWorld.MOD_ID,name),item);
+    }
+
+    public static void  registerModItems() {
+        ForgeWorld.LOGGER.info("Registering ModItems for" + ForgeWorld.MOD_ID);
+    }
+}
